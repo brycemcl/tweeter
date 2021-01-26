96149b14752c86e2feaf52ed7558f3e22ab46cba
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-26 18:10:05 +0000

    Refactored to allow parent query selector to be changed.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,17 @@
-window.addEventListener('load', (event) => {
-  document.querySelector('#tweet-text').addEventListener('input', (event) => {
-    const remainingCharacters = (140 - document.querySelector('#tweet-text').value.length);
-    const counter = document.querySelector("#counter");
-    if (remainingCharacters >= 0) {
-      counter.classList.remove("over-characters");
-    } else {
-      counter.classList.add("over-characters");
-    }
-    counter.value = remainingCharacters.toString();
-  });
+window.addEventListener("load", (event) => {
+  const updateCharacterCounter = (parentQuerySelector) => {
+    document
+      .querySelector(parentQuerySelector)
+      .addEventListener("input", function (event) {
+        const remainingCharacters = 140 - this.value.length;
+        const counter = document.querySelector("#counter");
+        if (remainingCharacters >= 0) {
+          counter.classList.remove("over-characters");
+        } else {
+          counter.classList.add("over-characters");
+        }
+        counter.value = remainingCharacters.toString();
+      });
+  };
+  updateCharacterCounter("#tweet-text");
 });
