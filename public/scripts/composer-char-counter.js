8e47722fa8337fb17c2cc3ff7040b86de4935aa5
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-26 17:47:17 +0000

    color changes based off css class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', (event) => {
   document.querySelector('#tweet-text').addEventListener('input', (event) => {
-    document.querySelector("#counter").value = (140 - document.querySelector('#tweet-text').value.length).toString();
+    const remainingCharacters = (140 - document.querySelector('#tweet-text').value.length);
+    const counter = document.querySelector("#counter");
+    if (remainingCharacters >= 0) {
+      counter.classList.remove("over-characters");
+    } else {
+      counter.classList.add("over-characters");
+    }
+    counter.value = remainingCharacters.toString();
   });
 });
